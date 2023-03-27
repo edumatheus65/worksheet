c0ea811b1bd043a6e58f7cbf01a0f60d8266a161
edumatheus65 <ematheusneves13@gmail.com>
2023-03-27 12:07:45 -0300

    refatorando codigo

--- worksheet/src/Table/TableHeader.js
+++ worksheet/src/Table/TableHeader.js
@@ -18,20 +18,4 @@ function TableHeader( { columns } ) {
 
 
 
-// function TableHeader( { columns } ) {
-//     const keys = Object.keys(columns.length?columns[0]:{});
-//     return (
-
-//         <thead>
-//             <tr>
-//                 {
-//                     keys.map(key => <th key={key}>{columns[key] || key}</th>)
-//                 }
-//             </tr>
-//         </thead>
-
-//     )
-
-// }
-
 export default TableHeader
