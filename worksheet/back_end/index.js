a343e3fbbeb5a69a0b0c326b5216ecf3d097b925
edumatheus65 <ematheusneves13@gmail.com>
2023-04-12 10:43:45 -0300

    Foi criado a pasta backend

---
+++ worksheet/back_end/index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const cors = require('cors')
+
+const app = express()
+const port = 7000
+const data = require('./data/wordList.json')
+const data_2 = require('./data_2/updateList.json')
+const data_3 = require('./data_3/db.json')
+
+app.use(cors())
+
+// const path = require("path")
+
+// const basePath = path.join(__dirname, 'data')
+
+app.get('/data', (req, res) => {
+    res.json(data)
+})
+
+app.get('/data/data_2', (req, res) => {
+    res.json(data_2)
+})
+
+// app.get('/data/db', (req, res) => {
+//     res.json(data_3)
+// })
+app.listen(port, () => {
+    console.log(`Rodando na porta ${port}`)
+})
